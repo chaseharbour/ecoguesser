f175bd379a806c6ffe0a37cf07c87290b5586f22
chaseharbour <harbourcp@gmail.com>
2022-03-07 17:32:11 -0800

    prog(): Began fleshing out project structure.

---
+++ pages/game.js
@@ -0,0 +1,13 @@
+import React from "react";
+import LinkButton from "./components/link-button";
+
+const Game = () => {
+  return (
+    <>
+      <h1>Game</h1>
+      <LinkButton location="/" label="Take me home" />
+    </>
+  );
+};
+
+export default Game;
