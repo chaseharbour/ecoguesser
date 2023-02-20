f345314e15e3e1372f04659327d12af7fb8b6ba8
chaseharbour <harbourcp@gmail.com>
2023-02-20 08:32:39 -0800

    fix(/utils/newGameSetup.js): Added comments to give better understanding of function.

--- utils/newGameSetup.js
+++ utils/newGameSetup.js
@@ -2,8 +2,10 @@ const { fetchLatLongData } = require("./getLatLongEndpoint");
 const { fetchObservationData } = require("./getObservationsEndpoint");
 
 const handleNewGameSetup = async () => {
+  //Gets random latitude and longitude from third party API
   let latLong = await fetchLatLongData();
 
+  //Checks for a list of organisms found near the random latitude and longitutde
   let check_res = await fetchObservationData(
     latLong.data.major.latt,
     latLong.data.major.longt
@@ -11,18 +13,12 @@ const handleNewGameSetup = async () => {
 
   console.log(check_res);
 
+  //Makes sure at least 20 organisms are found before making game playable
   if (check_res.data.total_results < 20) {
-    //   latLong = await fetchLatLongData();
-    //   check_res = await fetchObservationData(
-    //     latLong.data.major.latt,
-    //     latLong.data.major.longt
-    //   );
-
-    //   console.log(check_res);
-
     return await handleNewGameSetup();
   }
 
+  //Returns latitude and longitude to be used
   return latLong;
 };
 
